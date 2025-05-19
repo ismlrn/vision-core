2dd6d43ac10cb7e8835bd8e11056dc2a684a3508
ismlrn <ismail.rahmanikhah@gmail.com>
2025-05-19 06:11:36 +0330

    Designing model structure

--- main.py
+++ main.py
@@ -23,3 +23,28 @@ testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=32,
                                          shuffle=False)
+# defining the model a simple CNN
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        # adding convolutional layer
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        # adding max pooling
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        # fully connected layer
+        self.fc1 = nn.Linear(32 * 8 * 8, 128)
+        self.fc2 = nn.Linear(128, 10)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.pool(x)
+        x = self.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.view(-1, 32 * 8 * 8)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+model = SimpleCNN()
